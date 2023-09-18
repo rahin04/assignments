cec6a3cc384dc1e2825e795f28e5c16c5a449245
Rahin <rahin582@gmail.com>
2023-09-18 23:56:03 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,159 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(const myApp());
+void main() {
+  runApp(PhotoGalleryApp());
 }
 
-class myApp extends StatelessWidget{
-  const myApp({super.key});
-
+class PhotoGalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:homeActivity());
-  }
-
-}
-
-class homeActivity extends StatelessWidget{
-  const homeActivity({super.key});
-
-  //final items = [
-   // (icon: Icons.shopping_basket, name: 'Apples'),
-   // (icon: Icons.shopping_basket, name: 'Bananas'),
-   // (icon: Icons.shopping_basket, name: 'Milk'),
-  //  (icon: Icons.shopping_basket, name: 'Bread'),
-  //  (icon: Icons.shopping_basket, name: 'Eggs'),
- // ];
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("My shopping List"),
-          titleSpacing: 0,
-          centerTitle: true,
-          toolbarHeight: 100,
-          elevation: 70,
-          backgroundColor: Colors.blue,
-
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white), onPressed: (){},)
-          ],
-
-        ),
-        body: ListView(
-          children:[
-              ListTile(
-
-                title: Text("Apples"),
-                leading: Icon(Icons.shopping_basket),
-
-              ),
-              ListTile(
-
-                title: Text("Bananas"),
-                leading: Icon(Icons.shopping_basket),
-
-              ),
-               ListTile(
-
-                 title: Text("Bread"),
-                 leading: Icon(Icons.shopping_basket),
-
-            ),
-            ListTile(
-
-              title: Text("Milk"),
-              leading: Icon(Icons.shopping_basket),
-
-            ),
-            ListTile(
-
-              title: Text("Eggs"),
-              leading: Icon(Icons.shopping_basket),
-
-            ),
-          ],
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PhotoGalleryScreen(),
     );
   }
 }
 
+class PhotoGalleryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Welcome to our Photo Gallery!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Search for photos here...',
+                  border: OutlineInputBorder(),
 
 
+                ),
+              ),
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+              itemCount: 6,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Image Clicked!'),
+                      ),
+                    );
+                  },
+                  child: Container(
+
+                    child: Column(
+                      children: [
+                        Image.network(
+                          'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
+                          height: 80,
+                          width: 80,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Image ${index + 1}'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage('https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
+                    ), // Replace with your image URL
+                  ),
+                ),
+              ),
+              title: Text('Image 1'), // Replace with your own title
+              subtitle: Text('title and subtitle.'),
+            ),
+
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage('https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
+                    ), // Replace with your image URL
+                  ),
+                ),
+              ),
+              title: Text('Image 2'), // Replace with your own title
+              subtitle: Text('title and subtitle.'),
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage('https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
+                    ), // Replace with your image URL
+                  ),
+                ),
+              ),
+              title: Text('Image 3'), // Replace with your own title
+              subtitle: Text('title and subtitle.'),
+            ),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 150),
+                child: FloatingActionButton(
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: Text('Photos Uploaded Successfully!'),
+                      backgroundColor: Colors.blue,
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+
+
+                  child: Icon(Icons.upload, size: 35), // Use a valid Flutter icon here
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
