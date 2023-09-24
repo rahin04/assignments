9a2f1000bbd9c5d36054c89de0eebb32321c2df6
Rahin <rahin582@gmail.com>
2023-09-24 23:27:50 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,100 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhotoGalleryApp());
+  runApp(MyApp());
 }
 
-class PhotoGalleryApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: PhotoGalleryScreen(),
+      home: Counter(),
     );
   }
 }
 
-class PhotoGalleryScreen extends StatelessWidget {
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CounterState();
+  }
+}
+
+class CounterState extends State<Counter> {
+  int count = 0;
+
+  void incrementCount() {
+    setState((){
+      count++;
+      if (count >= 5) {
+        _showDialog();
+      }
+    });
+  }
+
+  void decrementCount() {
+    setState((){
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Button pressed $count times."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photo Gallery'),
-        centerTitle: true,
+        title: Text('Counter App'),
       ),
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                'Welcome to our Photo Gallery!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Count:',
+              style: TextStyle(fontSize: 20),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Search for photos here...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
+            Text(
+              '$count',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemCount: 6,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Image Clicked!'),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Image.network(
-                          'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
-                          height: 80,
-                          width: 80,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text('Image ${index + 1}'),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
-                    ), // Replace with your image URL
-                  ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: incrementCount,
+                  child: Text('+',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 ),
-              ),
-              title: Text('Image 1'), // Replace with your own title
-              subtitle: Text('title and subtitle.'),
-            ),
-            ListTile(
-              leading: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
-                    ), // Replace with your image URL
-                  ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: decrementCount,
+                  child: Text('-',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 ),
-              ),
-              title: Text('Image 2'), // Replace with your own title
-              subtitle: Text('title and subtitle.'),
-            ),
-            ListTile(
-              leading: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg',
-                    ), // Replace with your image URL
-                  ),
-                ),
-              ),
-              title: Text('Image 3'), // Replace with your own title
-              subtitle: Text('title and subtitle.'),
-            ),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 150),
-                child: FloatingActionButton(
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: Text('Photos Uploaded Successfully!'),
-                      backgroundColor: Colors.blue,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-
-                  child: Icon(Icons.upload,
-                      size: 35), // Use a valid Flutter icon here
-                ),
-              ),
+              ],
             ),
           ],
         ),
