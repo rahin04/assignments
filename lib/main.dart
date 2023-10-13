d02f384b63edaa8c5e7d582118eb3e79928b2ec5
Rahin <142527272+rahin04@users.noreply.github.com>
2023-10-13 23:42:52 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'MainScreen.dart';
 
-main(){
-  runApp(const myApp());
+
+void main() {
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget{
-  const myApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return const MaterialApp(home:homeActivity());
-  }
-
-}
-
-class homeActivity extends StatelessWidget{
-  const homeActivity({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-          titleSpacing: 0,
-          centerTitle: true,
-          toolbarHeight: 100,
-          elevation: 70,
-          backgroundColor: Colors.green,
-          leading: Icon(Icons.add_business),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.search, color: Colors.white), onPressed: (){},)
-          ],
-
-        ),
-        body: Center(child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("This is a mod 5 assignment", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "My ",
-                    style: TextStyle(fontSize: 20, color: Colors.red),
-                  ),
-                  TextSpan(
-                    text: "phone ",
-                    style: TextStyle(fontSize: 15, color: Colors.blue),
-                  ),
-                  TextSpan(
-                    text: "name ",
-                    style: TextStyle(fontSize: 16, color: Colors.purple),
-                  ),
-
-                  TextSpan(
-                    text: "Your ",
-                    style: TextStyle(fontSize: 20, color: Colors.orange),
-                  ),
-                  TextSpan(
-                    text: "phone ",
-                    style: TextStyle(fontSize: 20, color: Colors.orange),
-                  ),
-                  TextSpan(
-                    text: "name",
-                    style: TextStyle(fontSize: 20, color: Colors.orange),
-                  ),
-                ],
-              ),
-            ),
-
-          ],
-        )
-        )
+    return MaterialApp(
+      home: MainScreen(),
     );
   }
-
-
 }
-
 
